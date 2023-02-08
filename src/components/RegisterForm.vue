24506e593dc15f3210cf81bbfe05f64d262f2ede
Alexnander Nasokin <alexnas2python@gmail.com>
2023-02-08 23:50:50 +0300

    Save user data into UserCollection in Firebase.

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -108,7 +108,7 @@
 </template>
 
 <script>
-import { auth } from "@/includes/firebase";
+import { auth, usersCollection } from "@/includes/firebase";
 
 export default {
   name: "RegisterForm",
@@ -145,6 +145,21 @@ export default {
           values.email,
           values.password
         );
+      } catch (error) {
+        this.reg_in_submission = false;
+        this.reg_alert_variant = "bg-red-500";
+        this.reg_alert_message =
+          "An unexpected error occured. Please try again later.";
+        return;
+      }
+
+      try {
+        usersCollection.add({
+          name: values.name,
+          email: values.email,
+          age: values.age,
+          country: values.country,
+        });
       } catch (error) {
         this.reg_in_submission = false;
         this.reg_alert_variant = "bg-red-500";
