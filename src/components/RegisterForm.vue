d4b478585bd30f01c444d54112386e89f994711c
Alexnander Nasokin <alexnas2python@gmail.com>
2023-02-08 21:54:09 +0300

    Make user registration in the Firebase

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -108,6 +108,8 @@
 </template>
 
 <script>
+import firebase from "@/includes/firebase";
+
 export default {
   name: "RegisterForm",
   data() {
@@ -131,11 +133,24 @@ export default {
     };
   },
   methods: {
-    register(values) {
+    async register(values) {
       this.reg_show_alert = true;
       this.reg_in_submission = true;
       this.reg_alert_variant = "bg-blue-500";
       this.reg_alert_message = "Please wait! Your account is being created.";
+
+      let userCredentials = null;
+      try {
+        userCredentials = await firebase
+          .auth()
+          .createUserWithEmailAndPassword(values.email, values.password);
+      } catch (error) {
+        this.reg_in_submission = false;
+        this.reg_alert_variant = "bg-red-500";
+        this.reg_alert_message =
+          "An unexpected error occured. Please try again later.";
+        return;
+      }
 
       this.reg_alert_variant = "bg-green-500";
       this.reg_alert_message = "Success! Your account has been created.";
