b6c8ca188a6527633c89a865cfc9321b9e97a91d
Alexnander Nasokin <alexnas2python@gmail.com>
2023-02-09 15:39:48 +0300

    Persist authentication

--- src/App.vue
+++ src/App.vue
@@ -275,11 +275,23 @@
 <script>
 import AppHeader from "./components/Header.vue";
 import AppAuth from "./components/Auth.vue";
+import { mapWritableState } from "pinia";
+import useUserStore from "@/stores/user";
+import { auth } from "./includes/firebase";
+
 export default {
   name: "App",
   components: {
     AppHeader,
     AppAuth,
+  },
+  computed: {
+    ...mapWritableState(useUserStore, ["userLoggedIn"]),
+  },
+  created() {
+    if (auth.currentUser) {
+      this.useUserStore = true;
+    }
   },
 };
 </script>
