6d3a5e7c30578f690686b717d0de05238507a523
Alexnander Nasokin <alexnas2python@gmail.com>
2023-02-10 13:22:03 +0300

    Customize links

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import AboutView from "../views/AboutView.vue";
+import ManageView from "../views/ManageView.vue";
 
 const routes = [
   {
@@ -11,11 +12,16 @@ const routes = [
     path: "/about",
     component: AboutView,
   },
+  {
+    path: "/manage",
+    component: ManageView,
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  linkExactActiveClass: "text-yellow-500",
 });
 
 export default router;
