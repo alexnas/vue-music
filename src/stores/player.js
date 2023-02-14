0a807b7c67d190a7ce204b78d58f1522284a410b
Alexnander Nasokin <alexnas2python@gmail.com>
2023-02-15 01:13:09 +0300

    Player progress bar

--- src/stores/player.js
+++ src/stores/player.js
@@ -8,9 +8,14 @@ export default defineStore("player", {
     sound: {},
     seek: "00:00",
     duration: "00:00",
+    playerProgress: "0%",
   }),
   actions: {
     async newSong(song) {
+      if (this.sound instanceof Howl) {
+        this.sound.unload();
+      }
+
       this.currentSong = song;
 
       this.sound = new Howl({
@@ -37,6 +42,10 @@ export default defineStore("player", {
     progress() {
       this.seek = helper.formatTime(this.sound.seek());
       this.duration = helper.formatTime(this.sound.duration());
+
+      this.playerProgress = `${
+        (this.sound.seek() / this.sound.duration()) * 100
+      }%`;
 
       if (this.sound.playing()) {
         requestAnimationFrame(this.progress);
